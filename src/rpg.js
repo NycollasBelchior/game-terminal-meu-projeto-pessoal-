945d5845d02ad2121a99952e70f118ee627fb126
Nycollas Belchior <nyck2102@gmail.com>
2026-06-28 13:41:31 -0300

    organização com funções e melhorando o while

--- src/rpg.js
+++ src/rpg.js
@@ -1,199 +1,213 @@
-import leia from 'readline-sync'
+import leia from "readline-sync";
 
-console.log("----jogo de trabalho----")
-let nome = leia.question("digite seu nome: ");
+let jogador = {
+ dinheiro : 0,
+ salario : 100,
+ energiaG : 1,
+ energiamax : 1,
+ energia : 1,
+ empregoatual : "desempregado",
+ dias : 0,
 
-console.log("você quer começar o jogo?")
+ cursos : {
+    cursoPr : false,
+    cursoEn : false
+}
+}
 
-let resposta = leia.keyInSelect(["sim","não"]);
-let salario = 100
-let dinheiro =  0;
-let energia = 3;
-let maxenergia = 3;
-let gastoEnergia = 1
-let dia = 0;
-let cursoPgm = false
-let cursoEng = false
-
-
-if (resposta === 0){
-    console.log("você começou o jogo!");
-    console.log("----MENU----");
-    console.log("dinheiro: "+ dinheiro  );
-    console.log("energia: " + energia + "/"+ maxenergia);
-    console.log("")
-
-    
-
-  while (true) {
-    console.log("\nDinheiro: R$" + dinheiro);
-    console.log("energia: "+ energia + "/" + maxenergia);
-    console.log("salario: "+ salario);
-    console.log("dias: "+ dia)
+ 
 
 
-    let menu = leia.keyInSelect(["trabalhar", "loja","descansar","empregos","academia(+energia)","cursos"],"menu");
+
+//---------------------
+// LOOP DO JOGO
+//---------------------
+while (true) {
+    render();
+
+    let menu = leia.keyInSelect(
+        ["trabalhar", "descansar", "empregos", "treino", "cursos"],
+        "O que deseja? "
+    );
 
     if (menu === 0) {
-        if(energia >= gastoEnergia ){
-        dia++;
-        dinheiro += salario;
-        energia -= gastoEnergia;
-        console.log("energia: "+ energia);
-        console.log("Você trabalhou e ganhou R$"+ salario);
-        }
-        else{
-            console.log("você não conseguiu trabalhar")}
-    }
-
+        trabalhar();
+    } 
     else if (menu === 1) {
-        let compra = leia.keyInSelect(["sofa(R$200)", "cama(R$300)"],"loja");
-
-        if (compra === 0) {
-            if (dinheiro >= 200) {
-                dinheiro -= 200;
-                console.log("Você comprou o sofá!");
-            } else {
-                console.log("Dinheiro insuficiente para o sofá!");
-            }
-        }
-
-        else if (compra === 1) {
-            if (dinheiro >= 300) {
-                dinheiro -= 300;
-                console.log("Você comprou a cama!");
-            } else {
-                console.log("Dinheiro insuficiente para a cama!");
-            }
-        }
-    }
-else if (menu === 2) {
-    if (energia < maxenergia) {
-        energia += 1;
-        dia++;
-        console.log("Você descansou.");
-    } else {
-        console.log("Energia já está no máximo.");
-    }
-}
-else if (menu === 3){
-    let empregos = leia.keyInSelect(["repositor","taxi","estagiario(programação)","pedreiro","engenharia"],"empregos")
-    if(empregos === 0){
-        salario = 250;
-        gastoEnergia = 2;
-        console.log("você agora é um repositor!");
-        console.log("agora você recebe o salario de: " + salario);
-    }
-    else if (empregos === 1){
-        salario = 300;
-        gastoEnergia = 2;
-        console.log("você agora é um taxi!");
-        console.log("agora você recebe o salario de: "+ salario)
-
-    }
-    else if (empregos === 2){
-
-        if(dinheiro >= 1000 && cursoPgm === true){
-            gastoEnergia = 2;
-            salario = 650;
-            console.log("você agora trabalha num estágio dev");
-            console.log("você agora recebe o salário de: "+ salario);
-        }
-        else{
-            console.log("você precisa de R$1000 para conseguir este emprego");
-            console.log("você precisa do curso de programador (R$600)");
-        }
-
-
-    }
-    else if(empregos === 3){
-        gastoEnergia = 3;
-        salario = 500;
-        console.log("você agora é um pedreiro!");
-        console.log("você agora recebe o salário de: "+ salario);
-    }else if(empregos === 4){
-        if (cursoEng === true){
-        
-        gastoEnergia = 2;
-        salario = 650;
-        console.log("você é engenheiro");
-        console.log("você agora recebe o salario de: " + salario)
-        }
-        else {
-            console.log("você precisa do curso de engenharia!")
-        }
-
-    }
-    else if (empregos === -1){
-        console.log("você desistiu de procurar um trabalho!");
-    }
-}
-else if (menu ===4 ){
-    console.log("você deseja fazer a diária na academia?")
-    console.log("benefícios: +1 energia por dia pagado");
-    console.log("custo: R$150,00")
-    let resposta1 = leia.keyInSelect(["sim","não"]);
-    if(resposta1 === 0  && dinheiro >= 150){
-        dia++;
-        dinheiro -= 150;
-        maxenergia +=1;
-        console.log("você fez academia")
-        console.log("energia maxima agora: "+ maxenergia)
-    }
-    else if(resposta1 === 0 && dinheiro <150){
-        console.log("você não tem dinheiro suficiente")
-    }
-    else{
-        console.log("você não quis fazer academia")
-    }
-
-} else if(menu === 5){
-    console.log("você deseja fazer algum curso?")
-    let curso = leia.keyInSelect(["sim","não"]);
-    if (curso === 0){
-        let cursos = leia.keyInSelect(["programação(R$600)","engenharia(R$550)"]);
-
-        if (cursos === 0){
-            
-            if (dinheiro >= 600){
-                dinheiro -= 600
-                console.log("você fez curso!")
-                console.log("você liberou o cargo de dev.programador!")
-                cursoPgm = true
-            }
-            else if (dinheiro < 600){
-                console.log("dinheiro insuficiente");
-            }
-        }
-        else if (cursos === 1){
-            if(dinheiro >= 550){
-                dinheiro -= 550
-                console.log("você fez curso!")
-                console.log("você liberou o cargo de Engenheiro!")
-                cursoEng = true
-            }
-            else if (dinheiro < 550){
-                console.log("dinheiro insuficiente")
-            }
-        }
-        
-    }    
-    else{
-        console.log("você desistiu de fazer o curso")
-    }
-    }
-
+        descansar();
+    } 
+    else if (menu === 2) {
+        empregos();
+    } 
+    else if (menu === 3) {
+        treino();
+    } 
+    else if (menu === 4) {
+        curso();
+    } 
     else if (menu === -1) {
-        console.log("Jogo encerrando...");
+        console.log("Jogo encerrado");
         break;
     }
 }
+
+//---------------------
+// FUNÇÕES
+//---------------------
+
+function status() {
+    console.log("--------------------------");
+    console.log("dias: " + jogador.dias);
+    console.log("energia: " + jogador.energia + "/" + jogador.energiamax);
+    console.log("dinheiro: " + jogador.dinheiro);
+    console.log("salario: " + jogador.salario);
+    console.log("emprego: " + jogador.empregoatual);
+    console.log("--------------------------");
 }
 
+function render() {
+    console.clear();
+    status();
+}
 
+function pausa() {
+    leia.keyInPause("Pressione ENTER para continuar...");
+}
 
+//---------------------
+// AÇÕES
+//---------------------
 
+function trabalhar() {
+    if (jogador.energia >= jogador.energiaG) {
+        jogador.dinheiro += jogador.salario;
+        jogador.energia -= jogador.energiaG;
+        jogador.dias++;
 
+        console.log("Você trabalhou e ganhou: R$" + jogador.salario);
+    } else {
+        console.log("Energia insuficiente!");
+    }
 
+    pausa();
+}
 
+function descansar() {
+    if (jogador.energia < jogador.energiamax) {
+        jogador.energia++;
+        console.log("Você descansou!");
+    } else {
+        console.log("Energia já está no máximo!");
+    }
 
+    jogador.dias++;
+    pausa();
+}
+
+function empregos() {
+    let emprego = leia.keyInSelect([
+        "(R$150) pedreiro",
+        "(R$200) garçom",
+        "(R$650) programador",
+        "(R$700) engenheiro"
+    ]);
+
+    if (emprego === 0) {
+        jogador.salario = 150;
+        jogador.energiaG = 2;
+        jogador.empregoatual = "pedreiro";
+        console.log("Você agora trabalha como pedreiro!");
+    } 
+    else if (emprego === 1) {
+        if (jogador.energiamax >= 2) {
+            jogador.salario = 200;
+            jogador.energiaG = 2;
+            jogador.empregoatual = "garçom";
+            console.log("Você agora trabalha como garçom!");
+        } else {
+            console.log("Precisa de 2 energia máxima!");
+        }
+    } 
+    else if (emprego === 2) {
+        if (jogador.cursos.cursoPr && jogador.energiamax >= 3) {
+            jogador.salario = 650;
+            jogador.energiaG = 3;
+            jogador.empregoatual = "programador";
+            console.log("Você agora trabalha como programador!");
+        } else if (!jogador.cursos.cursoPr) {
+            console.log("Precisa do curso de programação!");
+        } else {
+            console.log("Precisa de 3 de energia máxima!");
+        }
+    } 
+    else if (emprego === 3) {
+        if (jogador.cursos.cursoEn && jogador.energiamax >= 3) {
+            jogador.salario = 700;
+            jogador.energiaG = 3;
+            jogador.empregoatual = "engenheiro";
+            console.log("Você agora trabalha como engenheiro!");
+        } else if (!jogador.cursos.cursoEn) {
+            console.log("Precisa do curso de engenharia!");
+        } else {
+            console.log("Precisa de 3 de energia máxima!");
+        }
+    }
+    jogador.dias++;
+    pausa();
+}
+
+function treino() {
+    console.log("Deseja treinar na academia por R$150? (+1 energia máxima)");
+
+    let resp = leia.keyInSelect(["sim", "não"]);
+
+    if (resp === 0) {
+        if (jogador.dinheiro >= 150) {
+            jogador.dinheiro -= 150;
+            jogador.energiamax++;
+            jogador.energia = jogador.energiamax;
+
+            console.log("Você treinou!");
+            console.log("Energia máxima: " + jogador.energiamax);
+        } else {
+            console.log("Dinheiro insuficiente!");
+        }
+    } else {
+        console.log("Você desistiu de treinar!");
+    }
+
+    jogador.dias++;
+    pausa();
+}
+
+function curso() {
+    let esc = leia.keyInSelect([
+        "(R$600) programação",
+        "(R$550) engenharia"
+    ]);
+
+    if (esc === 0) {
+        if (jogador.dinheiro >= 600) {
+            jogador.dinheiro -= 600;
+            jogador.cursos.cursoPr = true;
+            console.log("Curso de programação concluído!");
+        } else {
+            console.log("Dinheiro insuficiente!");
+        }
+    } 
+    else if (esc === 1) {
+        if (jogador.dinheiro >= 550) {
+            jogador.dinheiro -= 550;
+            jogador.cursos.cursoEn = true;
+            console.log("Curso de engenharia concluído!");
+        } else {
+            console.log("Dinheiro insuficiente!");
+        }
+    } 
+    else {
+        console.log("Você desistiu do curso!");
+    }
+
+    jogador.dias++;
+    pausa();
+}
